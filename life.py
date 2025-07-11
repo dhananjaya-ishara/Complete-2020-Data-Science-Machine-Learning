448c5f860f5c1e1d3e7552f3ec79096381c710ca
dhananjaya-ishara <167128344+dhananjaya-ishara@users.noreply.github.com>
2025-07-11 13:12:03 +0530

    Add files via upload

--- life.py
+++ life.py
@@ -1,1 +1,8 @@
-d=42
+import math
+
+def quote_marvin():
+  print('ive cal your chance of surival,but i don"t think you"ll like it')
+
+def square_root(number):
+  """Calculates the square root of a number."""
+  return math.sqrt(number)
